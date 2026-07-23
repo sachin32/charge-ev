ac8e72f4791daa65ba0389f4ed7902df56826ddd
Sachin Tiwari <sachin@lima.ai>
2026-07-23 14:45:36 +0530

    Added adsense code

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
-import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,14 +22,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
       <head>
-        <Script
-          id="google-adsense"
-          strategy="afterInteractive"
-          crossOrigin="anonymous"
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3925104231076381"
-          crossorigin="anonymous"
-        />
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3925104231076381"
+        crossorigin="anonymous"></script>
       </head>
       <body className="min-h-screen bg-[#F8FAFC] text-slate-900">
         <Header />
